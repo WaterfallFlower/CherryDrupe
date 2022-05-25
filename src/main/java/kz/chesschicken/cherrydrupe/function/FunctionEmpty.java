9b716dd0166a45da257611ddf2aa515f457ab1f1
ChessChicken-KZ <53552480+ChessChicken-KZ@users.noreply.github.com>
2022-05-26 02:13:40 +0600

    FunctionEmpty and FunctionObjectArray documentation.

--- src/main/java/kz/chesschicken/cherrydrupe/function/FunctionEmpty.java
+++ src/main/java/kz/chesschicken/cherrydrupe/function/FunctionEmpty.java
@@ -1,5 +1,10 @@
 package kz.chesschicken.cherrydrupe.function;
 
+/**
+ * An interface function with no arguments, but with return.
+ * @param <O> return type.
+ * @author ChessChicken-KZ
+ */
 public interface FunctionEmpty<O> {
     O apply();
 }
--- src/main/java/kz/chesschicken/cherrydrupe/function/FunctionObjectArray.java
+++ src/main/java/kz/chesschicken/cherrydrupe/function/FunctionObjectArray.java
@@ -1,5 +1,10 @@
 package kz.chesschicken.cherrydrupe.function;
 
+/**
+ * An interface function with arguments as Object array.
+ * @param <O> return type.
+ * @author ChessChicken-KZ
+ */
 @SuppressWarnings("UnusedReturnValue")
 public interface FunctionObjectArray<O> {
     O apply(Object... o);
