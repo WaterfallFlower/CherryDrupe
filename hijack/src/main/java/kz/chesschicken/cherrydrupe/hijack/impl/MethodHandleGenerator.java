a88289cfbabbd6b7f5de1587c09e52330d7ee170
ChessChicken-KZ <53552480+ChessChicken-KZ@users.noreply.github.com>
2022-08-31 14:36:51 +0600

    First refactor of MethodHandle's wrapper.

--- hijack/src/main/java/kz/chesschicken/cherrydrupe/hijack/impl/MethodHandleGenerator.java
+++ hijack/src/main/java/kz/chesschicken/cherrydrupe/hijack/impl/MethodHandleGenerator.java
@@ -16,88 +16,139 @@ import java.util.function.Function;
 import static kz.chesschicken.cherrydrupe.hijack.InstanceProvider.getLookup;
 
 /**
- * The implementation of {@link GlobalExceptionProcessor} with {@link java.lang.invoke.MethodHandles.Lookup}.
+ * The more specified and easy to use wrapper, designed specially for {@link java.lang.invoke.MethodHandles.Lookup} class.
+ *
  * @author ChessChicken-KZ
+ * @since 0.2
  */
 public class MethodHandleGenerator {
 
-    public static <T> @NotNull Function<@NotNull Object, @Nullable  T> field$Getter(@NotNull Class<?> source, @NotNull String field_name, @NotNull Class<T> field_type) {
+    public static <T> @NotNull Function<@NotNull Object, @Nullable T> generateFieldGetter(
+            @NotNull Class<?> source,
+            @NotNull String field_name,
+            @NotNull Class<T> field_type,
+            @Nullable Function<Throwable, T> onException
+    ) {
         return o -> {
             try {
                 //noinspection unchecked
                 return (T) getLookup().findGetter(source, field_name, field_type).invoke(o);
             } catch (Throwable e) {
-                GlobalExceptionProcessor.processException(e);
-                return GlobalExceptionProcessor.safeNullValue();
+                if(onException != null)
+                    return onException.apply(e);
+                e.printStackTrace();
+                return null;
             }
         };
     }
 
-    public static <T> @NotNull BiConsumer<@NotNull Object, @Nullable T> field$Setter(@NotNull Class<?> source, @NotNull String field_name, @NotNull Class<T> field_type) {
+    public static <T> @NotNull BiConsumer<@NotNull Object, @Nullable T> generateFieldSetter(
+            @NotNull Class<?> source,
+            @NotNull String field_name,
+            @NotNull Class<T> field_type,
+            @Nullable Consumer<Throwable> onException
+    ) {
         return (o, t) -> {
             try {
                 getLookup().findSetter(source, field_name, field_type).invoke(o, t);
             } catch (Throwable e) {
-                GlobalExceptionProcessor.processException(e);
+                if(onException != null)
+                    onException.accept(e);
+                else e.printStackTrace();
             }
         };
     }
 
-    public static <T> @NotNull FunctionRET<@Nullable T> fieldStatic$Getter(@NotNull Class<?> source, @NotNull String field_name, @NotNull Class<T> field_type) {
+    public static <T> @NotNull FunctionRET<@Nullable T> generateStaticFieldGetter(
+            @NotNull Class<?> source,
+            @NotNull String field_name,
+            @NotNull Class<T> field_type,
+            @Nullable Function<Throwable, T> onException
+    ) {
         return () -> {
             try {
                 //noinspection unchecked
                 return (T) getLookup().findStaticGetter(source, field_name, field_type).invoke();
             } catch (Throwable e) {
-                GlobalExceptionProcessor.processException(e);
-                return GlobalExceptionProcessor.safeNullValue();
+                if(onException != null)
+                    return onException.apply(e);
+                e.printStackTrace();
+                return null;
             }
         };
     }
 
-    public static <T> @NotNull Consumer<@Nullable T> fieldStatic$Setter(@NotNull Class<?> source, @NotNull String field_name, @NotNull Class<T> field_type) {
+    public static <T> @NotNull Consumer<@Nullable T> generateStaticFieldSetter(
+            @NotNull Class<?> source,
+            @NotNull String field_name,
+            @NotNull Class<T> field_type,
+            @Nullable Consumer<Throwable> onException
+    ) {
         return t -> {
             try {
                 getLookup().findStaticSetter(source, field_name, field_type).invoke(t);
             }catch (Throwable e) {
-                GlobalExceptionProcessor.processException(e);
+                if(onException != null) onException.accept(e);
+                else e.printStackTrace();
             }
         };
     }
 
-    public static <T> @NotNull FunctionARRSETRET<@Nullable T> method$Virtual(@NotNull Class<?> source, @NotNull String method_name, @NotNull Class<T> returnType, Class<?>... argumentsType) {
+    public static <T> @NotNull FunctionARRSETRET<@Nullable T> generateMethod(
+            @NotNull Class<?> source,
+            @NotNull String method_name,
+            @NotNull Class<T> returnType,
+            Class<?>[] params,
+            @Nullable Function<Throwable, T> onException
+    ) {
         return o -> {
             try {
                 //noinspection unchecked
-                return (T) getLookup().findVirtual(source, method_name, MethodType.methodType(returnType, argumentsType)).invokeWithArguments(Arrays.asList(o));
+                return (T) getLookup().findVirtual(source, method_name, MethodType.methodType(returnType, params)).invokeWithArguments(Arrays.asList(o));
             } catch (Throwable e) {
-                GlobalExceptionProcessor.processException(e);
-                return GlobalExceptionProcessor.safeNullValue();
+                if(onException != null)
+                    return onException.apply(e);
+                e.printStackTrace();
+                return null;
             }
         };
     }
 
-    public static <T> @NotNull FunctionARRSETRET<@Nullable T> method$Static(@NotNull Class<?> source, @NotNull String method_name, @NotNull Class<T> returnType, Class<?>... argumentsType) {
+    public static <T> @NotNull FunctionARRSETRET<@Nullable T> generateStaticMethod(
+            @NotNull Class<?> source,
+            @NotNull String method_name,
+            @NotNull Class<T> returnType,
+            Class<?>[] params,
+            @Nullable Function<Throwable, T> onException
+    ) {
         return o -> {
             try {
-                MethodHandle a = getLookup().findStatic(source, method_name, MethodType.methodType(returnType, argumentsType));
+                MethodHandle a = getLookup().findStatic(source, method_name, MethodType.methodType(returnType, params));
                 //noinspection unchecked
                 return (T) a.invokeWithArguments(Arrays.asList(o));
             } catch (Throwable e) {
-                GlobalExceptionProcessor.processException(e);
-                return GlobalExceptionProcessor.safeNullValue();
+                if(onException != null)
+                    return onException.apply(e);
+                e.printStackTrace();
+                return null;
             }
         };
     }
 
-    public static  <T> @NotNull FunctionARRSETRET<T> constructor$(@NotNull Class<?> source, Class<?>[] params) {
+    public static <T> @NotNull FunctionARRSETRET<T> generateConstructor(
+            @NotNull Class<?> source,
+            Class<?>[] params,
+            @Nullable Function<Throwable, T> onException
+    ) {
         return objectTypeArgs -> {
             try {
                 //noinspection unchecked
                 return (T) getLookup().findConstructor(source, MethodType.methodType(source, params)).invokeWithArguments(Arrays.asList(objectTypeArgs));
             } catch (Throwable e) {
-                GlobalExceptionProcessor.processException(e);
-                return GlobalExceptionProcessor.safeNullValue();
+                if(onException != null)
+                    return onException.apply(e);
+                e.printStackTrace();
+                return null;
             }
         };
     }
